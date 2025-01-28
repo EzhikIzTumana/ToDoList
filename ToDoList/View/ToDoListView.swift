605b218c2a636e6642f9213475b58218e7c8de90
EzhikIzTumana <sytko.maria@mail.ru>
2025-01-28 18:45:58 +0300

    Make a main page and add button

---
+++ ToDoList/View/ToDoListView.swift
@@ -0,0 +1,35 @@
+//
+//  ToDoListView.swift
+//  ToDoList
+//
+//  Created by Maria Sytsko on 28.01.25.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+    var body: some View {
+        NavigationView{
+            VStack {
+                
+            }
+            .navigationTitle("To Do List")
+            .toolbar{
+                Button {
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView){
+                NewItemView()
+            }
+                
+        }
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
---
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoListViewViewModel.swift
+//  ToDoList
+//
+//  Created by Maria Sytsko on 28.01.25.
+//
+
+import Foundation
+
+class ToDoListViewViewModel: ObservableObject{
+    @Published var showingNewItemView = false
+    init() {}
+}
